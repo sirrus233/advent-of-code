fcad0f36a097e10de8c41a1ceace973cd75f9ccb
Bradley Sherman <sirrus233@gmail.com>
2024-12-01 14:03:23 -0800

    Remove unnecessary hashmap

--- src/Solutions/Y2024/Day01.hs
+++ src/Solutions/Y2024/Day01.hs
@@ -3,7 +3,6 @@ module Solutions.Y2024.Day01 (solution1, solution2) where
 -- https://adventofcode.com/2024/day/1
 
 import Advent (Parser, Solution, lexeme)
-import Data.HashMap.Strict (findWithDefault)
 import Text.Megaparsec (parse)
 import Text.Megaparsec.Char.Lexer qualified as L
 
@@ -16,7 +15,6 @@ solution1 :: Solution Int
 solution1 input = sum . uncurry (zipWith (\a b -> abs (a - b))) . bimap sort sort . unzip <$> parse parser "" input
 
 solution2 :: Solution Int
-solution2 input = sum . (\(as, bs) -> map (\a -> a * count a bs) as) . second counter . unzip <$> parse parser "" input
+solution2 input = sum . (\(as, bs) -> map (\a -> a * count a bs) as) . unzip <$> parse parser "" input
   where
-    counter xs = fromList . map (\x -> (x, length . filter (== x) $ xs)) $ xs
-    count = findWithDefault 0
+    count a = length . filter (== a)
